2b3fef448230f87a74efbc751fc711340ff041cd
Jamie Meyer <45072324+HeatXD@users.noreply.github.com>
2024-04-22 04:08:14 +0200

    cleanup

--- dep/raylib-cpp-5.0.1/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: F:/Downloads/raylib-cpp-5.0.1/raylib-cpp-5.0.1
-# Build directory: F:/Downloads/raylib-cpp-5.0.1/raylib-cpp-5.0.1
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("include")
-subdirs("examples")
-subdirs("tests")
--- dep/raylib-cpp-5.0.1/_deps/raylib-build/raylib/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: F:/Downloads/raylib-cpp-5.0.1/raylib-cpp-5.0.1/_deps/raylib-src/src
-# Build directory: F:/Downloads/raylib-cpp-5.0.1/raylib-cpp-5.0.1/_deps/raylib-build/raylib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("external/glfw")
