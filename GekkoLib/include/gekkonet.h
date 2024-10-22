bbf8b745ddd9b6f48dbe2651b932b2564969854c
Jamie Meyer <jamiexxl5@hotmail.com>
2024-10-22 21:39:45 +0200

    Update CMakeLists

--- GekkoLib/include/gekkonet.h
+++ GekkoLib/include/gekkonet.h
@@ -1,11 +1,19 @@
 #pragma once
 
 #ifdef _WIN32
-#  ifdef GEKKONET_DLL_EXPORT  // Defined when building the DLL
-#    define GEKKONET_API __declspec(dllexport)
-#  else                      // Defined when using the DLL
-#    define GEKKONET_API __declspec(dllimport)
-#  endif
+#ifdef GEKKONET_STATIC
+// Static library - no import/export needed
+#define GEKKONET_API
 #else
-#  define GEKKONET_API  // Empty for non-Windows platforms
+#ifdef GEKKONET_DLL_EXPORT
+// Building the DLL
+#define GEKKONET_API __declspec(dllexport)
+#else
+// Using the DLL
+#define GEKKONET_API __declspec(dllimport)
+#endif
+#endif
+#else
+// Non-Windows platforms don't need special export macros
+#define GEKKONET_API
 #endif
