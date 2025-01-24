f40f5d683f0b0ad57674b59f32d5f2a2e4829253
Jamie Meyer <45072324+HeatXD@users.noreply.github.com>
2025-01-24 20:01:49 +0000

    update readme and license year

--- GekkoLib/include/gekkonet.h
+++ GekkoLib/include/gekkonet.h
@@ -1,7 +1,7 @@
 /*
 BSD 2-Clause License
 
-Copyright (c) 2024, Jamie Meyer
+Copyright (c) 2024-2025, Jamie Meyer
 
 Redistribution and use in source and binary forms, with or without
 modification, are permitted provided that the following conditions are met:
