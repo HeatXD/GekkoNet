4242ecb8daa66281517f26ae928f304032e3418e
nonk123 <me@nonk.dev>
2025-08-31 19:30:40 +0300

    Use the correct disconnection mechanism

--- GekkoLib/src/gekko.cpp
+++ GekkoLib/src/gekko.cpp
@@ -105,9 +105,10 @@ i32 Gekko::Session::AddActor(GekkoPlayerType type, GekkoNetAddress* addr)
 void Gekko::Session::RemoveActor(GekkoNetAddress addr)
 {
     NetAddress net_addr(addr.data, addr.size);
-    for (const auto& remote : _msg.remotes) {
-        if (remote->address.Equals(net_addr)) {
-            remote->SetStatus(Disconnected);
+    for (const auto& player : _msg.remotes) {
+        if (player->address.Equals(net_addr)) {
+            _msg.session_events.AddPlayerDisconnectedEvent(player->handle);
+            player->SetStatus(Disconnected);
             return;
         }
     }
